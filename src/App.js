febdf94cc02bc69b8086df769ab2eb2d8dfd94d8
Nataliya Glazyrina <stevep307@gmail.com>
2018-08-04 15:50:49 +0300

    Some linter issues fixed for udacity files.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     showSearchPage: false
-  }
+  };
 
   render() {
     return (
